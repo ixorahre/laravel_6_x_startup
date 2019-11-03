eac99171af92d7ae47d698e7c54bd0cfe990ecc7
Habib <hrekns@gmail.com>
2019-11-03 16:41:50 +0600

    laravel 6.1.4 project added with bootstrap 4 for the master

---
+++ resources/views/layouts/admin_master.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Habibur Rahman">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js"
+            integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>
+
+    <title>Laravel Start up</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+
+    @yield('style')
+</head>
+
+<body>
+
+@include('partials.nav')
+
+<main role="main" class="container">
+    <div class="container main-container-part">
+        @yield('content')
+    </div>
+</main><!-- /.container -->
+
+<div class="container">
+    @include('partials.footer')
+</div>
+
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+
+@yield('script')
+</body>
+</html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,18 @@
+<!-- Footer -->
+<footer class="page-footer footer">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <p class="py-3">Created by ixora solution</p>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <!-- Copyright -->
+            <div class="footer-copyright text-right py-3">© 2019 ixora solution:
+                <a href="https://ixorasolution">ixora solution</a>
+            </div>
+            <!-- Copyright -->
+        </div>
+    </div>
+
+
+</footer>
+<!-- Footer -->
